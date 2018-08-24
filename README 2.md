86d8f22e501b3f7087c46c3b7278ddd80baee91a
Andrey Kirichenko <andrey.kirichenko999@gmail.com>
2018-08-24 21:28:06 +0300

    initial commit

---
+++ README 2.md
@@ -0,0 +1,31 @@
+# Webpack 4 simple start kit
+
+Webpack4 + Pug + es2015 + scss
+
+### Installing
+
+```
+npm i
+
+```
+
+### Running
+
+For production:
+
+```
+npm run build
+
+```
+
+For development:
+
+```
+npm run dev
+
+```
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+let path = require('path');
+let MiniCssExtractPlugin = require('mini-css-extract-plugin');
+let HtmlWebpackPlugin = require('html-webpack-plugin');
+let CopyWebpackPlugin = require('copy-webpack-plugin');
+
+let conf = {
+  entry: './src/js/app.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'app.js',
+    publicPath: 'dist/'
+  },
+  devServer: {
+    overlay: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.pug/,
+
+        use: [
+          'pug-loader'
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ],
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'app.css',
+    }),
+    new HtmlWebpackPlugin({
+      template : './src/pug/index.pug',
+      filename: '../index.html'
+    }),
+    new CopyWebpackPlugin([
+      { from: 'src/img/', to: 'img/', force: true },
+      { from: 'src/fonts/', to: 'fonts/', force: true }
+    ], {})
+  ],
+};
+
+module.exports = (env, options) => {
+  let production = options.mode === 'production';
+  conf.devtool = production ? false : 'eval-sourcemap';
+  return conf;
+};
